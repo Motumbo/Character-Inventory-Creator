11daccc0d04d70d00322cbed7d7a9822e011f484
Julian Cardonnet <julian.cardonnet@five9.com>
2015-02-28 19:57:06 -0300

    Agregado diccionario de atributos

--- Character Inventory Administrator/Modelo/AtributosModel.cs
+++ Character Inventory Administrator/Modelo/AtributosModel.cs
@@ -1,5 +1,8 @@
-﻿namespace Character_Inventory_Administrator.Modelo
+﻿using System.Collections.Generic;
+
+namespace Character_Inventory_Administrator.Modelo
 {
+    delegate int attributeGetter();
     public class AtributosModel
     {
         int fuerza;
@@ -58,6 +61,17 @@
             Inteligencia = i;
             Sabiduria = s;
             Carisma = ch;
+        }
+
+        public int getAtributosDict() {
+            //TODO: add getters
+            Dictionary<string, attributeGetter> dictionary = new Dictionary<string, attributeGetter>();
+            dictionary.Add("Fuerza", this.Fuerza);
+            dictionary.Add("Destreza", this.Destreza);
+            dictionary.Add("Constitucion", this.Constitucion);
+            dictionary.Add("Inteligencia", this.Inteligencia);
+            dictionary.Add("Sabiduria", this.Sabiduria);
+            dictionary.Add("Carisma", this.Carisma);
         }
     }
 }
